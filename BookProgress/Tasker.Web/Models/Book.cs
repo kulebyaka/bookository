0019b24f48453824abc3ca00c848088637918d90
Kirill Elizarov <kiril.elizarov@gmail.com>
2016-03-07 14:09:28 +0100

    Id added

--- BookProgress/Tasker.Web/Models/Book.cs
+++ BookProgress/Tasker.Web/Models/Book.cs
@@ -8,6 +8,7 @@ namespace Tasker.Web.Models
 {
     public class Book
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public Status Status;
